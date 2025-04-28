1c5e1a82ad13d427e465c5f6fc3f514d5fb1385d
Sumit Sharma <100469632+sumitkumarsharm@users.noreply.github.com>
2025-04-28 14:55:07 +0530

    Introduction to javascript how to make factory in javascript auth login and more

---
+++ DAY-08/index.js
@@ -0,0 +1,15 @@
+function printChai() {
+  console.log("Chai");
+}
+printChai();
+
+function brushLa(itne) {
+  return `Hanji, le aaya Brush : ${itne}`;
+}
+brushLa(4);
+
+// adding two number factory
+function add(a, b) {
+  return `Adding ${a} and ${b} is : ${a + b}`;
+}
+console.log(add(12, 45));
