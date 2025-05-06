764ce97a140990576e33628b63eef39e2c4e5950
Sumit Sharma <100469632+sumitkumarsharm@users.noreply.github.com>
2025-05-06 14:15:33 +0530

    Today I Learn classes in js and what is Prototype Inheritance

---
+++ DAY-12/JS-Object-Prototype.js
@@ -0,0 +1,26 @@
+class Person {
+  constructor(Name, Age) {
+    this.Name = Name;
+    this.Age = Age;
+  }
+
+  getFullName = function () {
+    return `${this.Name} ${this.Age}`;
+  };
+}
+
+const Obj2 = {
+  fname: "Anirudh",
+  lName: "Jawala",
+};
+
+const p1 = new Person("Hitesh", 29);
+console.log(p1.getFullName());
+
+// how to null the prototype
+
+// const Arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// Arr.__proto__ = null; //Don't do this
+
+// Arr.push(1);
+// console.log(Arr);
