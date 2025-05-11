c474ba11cccc524a05c0543f27ac6ab8b6f394d3
Sumit Sharma <100469632+sumitkumarsharm@users.noreply.github.com>
2025-05-11 15:07:51 +0530

    here we learn varible on fast recap

--- DAY-14/Variable.js
+++ DAY-14/Variable.js
@@ -1,103 +1,107 @@
-// let Name = "Sumit";
-// const PI = 3.14;
+let Name = "Sumit";
+const PI = 3.14;
 
-// // Data Type
-// // Primitive
-// let number = 525; //Number
-// let string = "Sumit"; //String
-// let isTrue = true; //Boolean
-// let nullValue = null; //Object
-// let undefinedValue; //undefined
-// let symbol = Symbol(); //Symbol
-// let BigInt = 1234567899897876554434566333n; //BigInt
+// Data Type
+// Primitive
+let number = 525; //Number
+let string = "Sumit"; //String
+let isTrue = true; //Boolean
+let nullValue = null; //Object
+let undefinedValue; //undefined
+let symbol = Symbol(); //Symbol
+let BigInt = 1234567899897876554434566333n; //BigInt
 
-// // non-Primitive
-// let person = {
-//   Name: "Sumit",
-//   Age: 24,
-//   isStudent: true,
-//   address: {
-//     city: "Bhopal",
-//     state: "Madhya Pradesh",
-//     pinCode: 462022,
-//     country: "India",
-//   },
-// };
+// non-Primitive
+let person = {
+  Name: "Sumit",
+  Age: 24,
+  isStudent: true,
+  address: {
+    city: "Bhopal",
+    state: "Madhya Pradesh",
+    pinCode: 462022,
+    country: "India",
+  },
+};
 
-// // Standered conversion
+// Standered conversion
 
-// // Number
+// Number
 
-// let Num1 = "87";
-// let converted1 = Number(Num1);
-// console.log(converted1); // 87 in number
-// console.log(typeof converted1); // number
+let Num1 = "87";
+let converted1 = Number(Num1);
+console.log(converted1); // 87 in number
+console.log(typeof converted1); // number
 
-// let Num2 = "87aa";
-// let converted2 = Number(Num2);
-// console.log(converted2); //NaN this is a response it is not a number
-// console.log(typeof converted2); //number
+let Num2 = "87aa";
+let converted2 = Number(Num2);
+console.log(converted2); //NaN this is a response it is not a number
+console.log(typeof converted2); //number
 
-// let Num3 = "87";
-// let converted3 = +Num3;
-// console.log(converted3); // 87
-// console.log(typeof converted3); // number
+let Num3 = "87";
+let converted3 = +Num3;
+console.log(converted3); // 87
+console.log(typeof converted3); // number
 
-// let Num4 = "87iu";
-// let converted4 = +Num4;
-// console.log(converted4); // NaN
-// console.log(typeof converted4); // number
+let Num4 = "87iu";
+let converted4 = +Num4;
+console.log(converted4); // NaN
+console.log(typeof converted4); // number
 
-// let Num5 = "87";
-// let converted5 = parseInt(Num5);
-// console.log(converted5); // 87
-// console.log(typeof converted5); // number
+let Num5 = "87";
+let converted5 = parseInt(Num5);
+console.log(converted5); // 87
+console.log(typeof converted5); // number
 
-// let Num6 = 99;
-// let converted6 = parseFloat(Num6);
-// console.log(converted6); // 87
-// console.log(typeof converted6); // number
+let Num6 = 99;
+let converted6 = parseFloat(Num6);
+console.log(converted6); // 87
+console.log(typeof converted6); // number
 
-// // String
+// String
 
-// let str = 525;
-// let converted = String(str);
-// console.log(converted);
-// console.log(typeof converted);
+let str = 525;
+let converted = String(str);
+console.log(converted);
+console.log(typeof converted);
 
-// // Operations on math
-// let a = 52;
-// let b = 52;
+// Operations on math
+let a = 52;
+let b = 52;
 
-// let result1 = a + b;
-// let result2 = a - b;
-// let result3 = a * b;
-// let result4 = a / b;
-// let result5 = a % b;
-// console.log(result1);
-// console.log(result2);
-// console.log(result3);
-// console.log(result4);
-// console.log(result5);
+let result1 = a + b;
+let result2 = a - b;
+let result3 = a * b;
+let result4 = a / b;
+let result5 = a % b;
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
 
-// // comparation Operator
-// let X = 34;
-// let Y = "87";
-// console.log(X === Y); // it  is check the data is equal and dataType also
-// console.log(X == Y); //  it is check the data is equal not it not check dataType
-// console.log(X != Y); //  it is check not equal to
-// console.log(X > Y);
-// console.log(X < Y);
-// console.log(X <= Y);
-// console.log(X >= Y);
+// comparation Operator
+let X = 34;
+let Y = "87";
+console.log(X === Y); // it  is check the data is equal and dataType also
+console.log(X == Y); //  it is check the data is equal not it not check dataType
+console.log(X != Y); //  it is check not equal to
+console.log(X > Y);
+console.log(X < Y);
+console.log(X <= Y);
+console.log(X >= Y);
 
-// // Js ke liye : Math or Date  ke sath khelo jindagi Assan hogi
-// // Node ke liye : crypto or http  ke sath khelo jindagi Assan hogi
-
-// ------
+// Js ke liye : Math or Date  ke sath khelo jindagi Assan hogi
+// Node ke liye : crypto or http  ke sath khelo jindagi Assan hogi
 
 // concatinating
 let firstName = "Sumit";
 let LastName = "Sharma";
 let fullName = firstName + " " + LastName;
 console.log(fullName);
+
+let email = `SumitSharma2748@gmail.com`;
+const convertedEmail = email.toLowerCase();
+console.log(convertedEmail);
+console.log(convertedEmail.indexOf("com"));
+console.log(convertedEmail.slice(0, 22));
---
+++ DAY-14/array.js
@@ -0,0 +1,32 @@
+const chaiArray = [
+  "lemon tea",
+  "Green tea",
+  "Chamomile tea",
+  "Oolong tea",
+  "White tea",
+  "Herbal tea",
+];
+// console.log(chaiArray);
+// console.log(`total type of tea : ${chaiArray}`);
+
+// let index = chaiArray.indexOf("Green tea");
+// if (index !== -1) {
+//   chaiArray.splice(index, 1);
+// }
+// console.log(chaiArray);
+
+// // push
+// chaiArray.push("Black tea");
+// console.log(chaiArray);
+
+// // pop
+// chaiArray.pop();
+// console.log(chaiArray);
+
+// // shift
+// chaiArray.shift();
+// console.log(chaiArray);
+
+// // unshift
+// chaiArray.unshift("Chamomile tea");
+// console.log(chaiArray);
