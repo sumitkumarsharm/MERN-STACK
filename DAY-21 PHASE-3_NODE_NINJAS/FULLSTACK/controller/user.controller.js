2eba73bcc9b6da2ab97244d0f7894bbe2240f656
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2025-08-20 13:35:20 +0530

    Delete backend folder

--- DAY-21 PHASE-3_NODE_NINJAS/FULLSTACK/controller/user.controller.js
+++
@@ -1,9 +0,0 @@
-const registerUser = async (req, res) => {
-  res.send("<h1>Good Afternoon ❤️ user Registered</h1>");
-};
-
-const login = async (req, res) => {
-  res.send("<h1>Good Afternoon ❤️ user login</h1>");
-};
-
-export { registerUser, login };
--- DAY-21 PHASE-3_NODE_NINJAS/FULLSTACK/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, registerUser } from "../controller/user.controller.js";
-
-const router = express.Router();
-
-router.get("/register", registerUser);
-router.get("/login", login);
-
-export default router;
