918bc2ab2b67c4709daf98bbbd99e58f856545e2
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2025-09-15 23:43:53 +0530

    cleaning the code

--- NodeNinjas/MegaProject/src/index.js
+++ NodeNinjas/MegaProject/src/index.js
@@ -1,24 +1,18 @@
-import app from "./app.js"
-import dotenv from "dotenv"
-import connectDB from "./db/db.connect.js"
+import app from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/db.connect.js";
 
-dotenv.config({
-    path : "./.env"
-})
+dotenv.config({ path: "./.env" });
 
-
-const PORT = process.env.PORT || 8080
-
+const PORT = process.env.PORT || 8080;
 
 connectDB()
-.then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`Server is running on http://localhost:${PORT}`)
-    })
-})
-.catch((err)=>{
-    console.error("Error in Connecting MongoDB",err)
-    process.exit(1)
-})
-
-
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error in Connecting MongoDB", err);
+    process.exit(1);
+  });
