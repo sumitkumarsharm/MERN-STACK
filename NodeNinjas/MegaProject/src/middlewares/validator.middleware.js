3ffba244ccd6c60112cb5e788c84bb4c4dc2985c
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2025-11-02 01:38:30 +0530

    create validatore middleware

---
+++ NodeNinjas/MegaProject/src/middlewares/validator.middleware.js
@@ -0,0 +1,14 @@
+import { validationResult } from "express-validator";
+import {ApiError} from "../utils/api-error.js"
+export const validate = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+    next();
+  }
+
+  const extractedErrors = [];
+  errors.array().map((err) => extractedErrors.push({ [err.path]: err.msg }));
+
+  throw new ApiError(422,"recived data is not valid",extractedErrors)
+};
