576a2a4a8dbf037d896996114a9948dab2ee13e3
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2025-11-02 14:13:51 +0530

    making the a managment system

---
+++ NodeNinjas/RepeateMegaProject/src/index.js
@@ -0,0 +1,20 @@
+import app from './app.js';
+import dotenv from 'dotenv';
+import connedDB from './db/DB.js';
+
+dotenv.config({
+    path:'./.env'
+});
+
+const PORT = process.env.PORT || 8000;
+
+connedDB()
+    .then(()=>{
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        });
+    })
+    .catch((err)=>{
+        console.log("Database Connection failed", err)
+    })
+
